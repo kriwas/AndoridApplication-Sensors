44bebfdf9bc9fe8aeaf8b8c6e3d2edbc1c54fc6c
MateuszHelinski <mateuszh12@gmail.com>
2015-03-11 22:52:32 +0100

    Accelerometer reads added

--- app/src/main/java/pl/helimat/androidsensorsedu/MainActivity.java
+++ app/src/main/java/pl/helimat/androidsensorsedu/MainActivity.java
@@ -1,17 +1,38 @@
 package pl.helimat.androidsensorsedu;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private TextView devListText;
+    List<Sensor> deviceSensors;
+    Sensor accelerometerSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        accelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        devListText = (TextView)findViewById(R.id.textView);
+        devListText.setText("");
     }
 
 
@@ -35,5 +56,18 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        if(sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            devListText.setText(Float.toString(event.values[0]) + "\n" + Float.toString(event.values[1]) + "\n" + Float.toString(event.values[2]) + "\n");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
